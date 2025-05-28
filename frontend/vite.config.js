41a1c32133fee886c7f29d351a6b93881c5ac200
Cynthia-M-M <cynthiamogaka49@gmail.com>
2025-05-28 22:09:46 +0300

    updated files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
